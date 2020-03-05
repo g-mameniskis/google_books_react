90a746c8df4d816956415a502215c98a76b08d49
unknown <g.mameniskis1@gmail.com>
2020-03-05 11:43:47 -0500

    update

--- src/Books.js
+++ src/Books.js
@@ -3,7 +3,7 @@ import SearchArea from './SearchArea';
 import request from 'superagent';
 import BookList from './BookList';
 
-import Axios from 'axios';
+import axios from 'axios';
 
 class Books extends Component {
   constructor(props){
@@ -16,11 +16,43 @@ class Books extends Component {
   }
 
   // TUTORTIAL METHOD FOR CALLING API DIRECTLY AND GETTING BACK DATA
+
+  // searchBook = (e) => {
+  //   e.preventDefault();
+  //   request
+  //     .get("https://www.googleapis.com/books/v1/volumes")
+  //     .query({ q: this.state.searchField })
+  //     .then((data) => {
+  //       console.log(data);
+  //       const cleanData = this.cleanData(data)
+  //       this.setState({ books: cleanData })
+  //     })
+  // }
+
+
+
+  
+  // searchBook = (e) => {
+  //   axios
+  //   .post("https://www.googleapis.com/books/v1/volumes")
+  //   .then(function(response){
+  //     console.log(response)
+  //   });
+  // }
+
+
   searchBook = (e) => {
     e.preventDefault();
     request
-      .get("https://www.googleapis.com/books/v1/volumes")
-      .query({ q: this.state.searchField })
+      .post("http://localhost:8080/book-query-result-controller/",   { example: 'data' })    
+      .send({
+        "domain": "https://www.googleapis.com/books/v1",
+        "searchCategory": "volumes",
+        "searchTerm": "sun also rises",
+        "searchTopic": "",
+        "searchTopicValue": "",
+        "apiKey":"AIzaSyDoc04NEgl3jof9iclXzaoXvKlTI3gRS38"
+      })
       .then((data) => {
         console.log(data);
         const cleanData = this.cleanData(data)
@@ -29,41 +61,40 @@ class Books extends Component {
   }
 
 
-  // NEW METHOD FOR UPDATING QUERY (PUT REQUEST ATTEMPT)
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch('http://localhost:8080/book-query-result-controller/', {
-      method: 'POST',
-      headers: {
-        'Accept':'application/json',
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify({
-        searchField: event.target.searchField.value //Need to figure out if 'searchField' is correct value
-      })
-    })
-    .then(res=> res.json())
-    .then((result)=>
-    {
-      console.log(data);
-      const cleanData = this.cleanData(data)
-      this.setState({ books: this.cleanData })
-    },
-    )
-  }
+   //AXIOS GET METHOD
+// function handleSubmit() {
+// axios.get("https://www.googleapis.com/books/v1/volumes")
+// .then(function(response){
+//   console.log(response)
+// });
+// }
 
-  // MY METHOD FOR UPDATING QUERY ON BACK-END WITH USER INPUT
-  // searchBook = (e) => {
-  //   e.preventDefault();
-  //   request
-  //     .get("http://localhost:8080/book-query-result-controller/")
-  //     .query({ q: this.state.searchField })
-  //     .then((data) => {
-  //       console.log(data);
-  //       const cleanData = this.cleanData(data)
-  //       this.setState({ books: cleanData })
+
+  // NEW METHOD FOR UPDATING QUERY (PUT REQUEST ATTEMPT)
+  // https://www.youtube.com/watch?v=ZUhuMS1dFh8
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   fetch('https://www.googleapis.com/books/v1/volumes', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept':'application/json',
+  //       'Content-Type':'application/json'
+  //     },
+  //     body:JSON.stringify({
+  //       searchField: event.target.searchField //Need to figure out if 'searchField' is correct value
   //     })
+  //   })
+  //   .then(res=> res.json())
+  //   .then((result)=>
+  //   {
+  //     console.log(result);
+  //     const cleanData = this.cleanData(result)
+  //     this.setState({ books: this.cleanData })
+  //   },
+  //   )
   // }
+
 
   handleSearch = (e) => {
     this.setState({ searchField: e.target.value })
@@ -99,6 +130,10 @@ class Books extends Component {
       else if(this.state.sort === 'Oldest') {
         return parseInt(a.volumeInfo.publishedDate.substring(0,4)) - parseInt(b.volumeInfo.publishedDate.substring(0,4))
       }
+
+      else {
+        return -1;
+      }
     })
     return (
       <div>
@@ -109,7 +144,12 @@ class Books extends Component {
   }
 }
 
-// AXIOS
-// Axios.post("httpgjgj", state.searchField)
+// //AXIOS GET METHOD
+// function handleSubmit() {
+// axios.get("https://www.googleapis.com/books/v1/volumes")
+// .then(function(response){
+//   console.log(response)
+// });
+// }
 
 export default Books;
